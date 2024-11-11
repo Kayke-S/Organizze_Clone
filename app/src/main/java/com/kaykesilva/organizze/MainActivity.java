609d4a68a7d339115e4ddf8a9fdc744f022c01fc
Kayke-S <kayke.allogins@gmail.com>
2024-11-10 23:09:26 -0300

    Starting Project and configuring Firebase

---
+++ app/src/main/java/com/kaykesilva/organizze/MainActivity.java
@@ -0,0 +1,23 @@
+package com.kaykesilva.organizze;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
